029472125d0a2350c7d066f7b629f0d1c4e05f55
Kristukas <60770967+Kristukas@users.noreply.github.com>
2020-04-24 22:42:33 +0300

    Add files via upload

---
+++ Files/Deque/Deque.cpp
@@ -0,0 +1,381 @@
+#include "v03.h"
+int main()
+{
+    ofstream fr("kursiokai.txt");
+    string failas0 = "Pirmas.txt";
+    string ats = "Atsakymai1.1.txt";
+    string ats1 = "Atsakymai1.2.txt";
+    deque <pazymiai> N;
+    int n, k = 1000, rekursija = 1, namudarbuskc = 5, rekursija2 = 1;
+    deque <pazymiai> M;
+    int x = 1, a = 0;
+    char MV;
+    string failas5;
+    srand(time(NULL));
+    cout << "Ar norite duomenis nuskaityt is failo?" << endl;
+    string nuskaitymas, line, dummyline;
+    bool nuskaitymas2 = false;
+    while (1)
+    {
+        cin >> nuskaitymas;
+        if (nuskaitymas == "taip")
+        {
+            nuskaitymas2 = true;
+            break;
+        }
+        else if (nuskaitymas == "ne")
+        {
+            nuskaitymas2 = false;
+            break;
+        }
+        else {
+            cin.clear();
+            cin.ignore(500, '\n');
+            cout << "KLAIDA. Irasykite 'taip' arba 'ne' " << endl;
+        }
+    }
+    bool nuskaitymas5 = false;
+    if (nuskaitymas2 == true)
+    {
+        cout << "Jei norite atlikti testavima spauskite 't', jei norite skaityti is turimo failo 'f'" << endl;
+        while (1)
+        {
+            cin >> failas5;
+            if (failas5 == "t")
+            {
+                nuskaitymas2 = false;
+                nuskaitymas5 = true;
+                break;
+            }
+            else if (failas5 == "f")
+            {
+                nuskaitymas5 = false;
+                break;
+            }
+            else {
+                cin.clear();
+                cin.ignore(500, '\n');
+                cout << "KLAIDA. Iveskite tinkama raide " << endl;
+            }
+        }
+    }
+    string failas;
+    ifstream fd;
+    if (nuskaitymas2 == true)
+    {
+        while (1)
+        {
+            if (nuskaitymas2 == true)
+            {
+                cout << "Iveskite failo pavadinima" << endl;
+                cin >> failas;
+            }
+            fd.open(failas);
+            try {
+                if (fd.is_open())
+                {
+                    throw(1);
+                }
+                else throw(1.0);
+            }
+            catch (int)
+            {
+                cout << "Failas rastas" << endl;
+                break;
+            }
+            catch (double)
+            {
+                cout << "Failas nerastras bandykite dar karta" << endl;
+            }
+        }
+    }
+    cout << "Jei galutini bala norite skauciuoti su mediana spauskite M, jei vidurkiu V " << endl;
+    while (1)
+    {
+        cin >> MV;
+        if (MV == 'M' || MV == 'V')
+        {
+            break;
+        }
+        else {
+            cin.clear();
+            cin.ignore(500, '\n');
+            cout << "KLAIDA. Iveskite tinkama raide " << endl;
+        }
+
+    }
+    string Skaityt;
+    using hrClock = std::chrono::high_resolution_clock;
+    std::mt19937 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
+    string Gavimas("Pirmas.txt");
+    int N1 = 0, B1 = 0;
+    if (nuskaitymas5 == true)
+    {
+        Gen(rekursija, k, failas0, namudarbuskc, mt);
+        Nuskaitymas(Gavimas, dummyline, line, M, a, MV, x, N, N1, B1, ats, ats1, rekursija2);
+    }
+    if (nuskaitymas2 == true)
+    {
+        getline(fd, dummyline);
+        while (getline(fd, line))
+        {
+            stringstream ss(line);
+            int numOfWords = countWords(line) - 3;
+            int exam, suma = 0;
+            double mediana = 0;
+            M.push_back(pazymiai());
+            ss >> Skaityt;
+            M[a].SetPavarde(Skaityt);
+            ss >> Skaityt;
+            M[a].SetVardas(Skaityt);
+            int temp;
+            for (int i = 0; i < numOfWords; i++)
+            {
+                ss >> temp;
+                suma += temp;
+                M[a].SetPazymys(temp); //
+            }
+            ss >> exam;
+            if (MV == 'V')
+                M[a].SetGalutinis(0.4 * (suma / numOfWords) + (0.6 * exam));
+            else if (MV == 'M')
+            {
+                M[a].Sort();
+                if (numOfWords % 2 != 0)
+                {
+                    int k = 0, kint;
+                    k = numOfWords / 2;
+                    kint = numOfWords;
+                    kint -= k;
+                    mediana = M[a].GetPazymys(kint - 1);
+                }
+                else
+                {
+                    int k = 0;
+                    k = numOfWords / 2;
+                    mediana = (M[a].GetPazymys(k - 1) + M[a].GetPazymys(k)) / 2;
+                }
+                M[a].SetGalutinis((0.4 * mediana) + (0.6 * exam));
+            }
+            a++;
+            x = a;
+        }
+    }
+    else if (nuskaitymas2 == false && nuskaitymas5 == false) {
+        string Kint;
+        for (int i = 0; i < x; i++)
+        {
+            M.push_back(pazymiai());
+            cout << "Iveskite studento varda" << endl;
+            cin >> Kint;
+            M[i].SetVardas(Kint);
+            cin.ignore();
+            cin.clear();
+            cout << "Iveskite studento pavarde" << endl;
+            cin >> Kint;
+            M[i].SetPavarde(Kint);
+            cout << "Ar norite namu darbu rezultatus sugeneruoti atsitiktinai?" << endl;
+            string atsakymas;
+            while (1)
+            {
+                cin >> atsakymas;
+                if (atsakymas == "taip" || atsakymas == "ne")
+                {
+                    break;
+                }
+                else {
+                    cin.clear();
+                    cin.ignore(500, '\n');
+                    cout << "KLAIDA. Irasykite 'taip' arba 'ne' " << endl;
+                }
+            }
+            int a = 0;
+            double kiekis = 0, mediana = 0;
+            double skc;
+            if (atsakymas == "ne")
+            {
+                cout << "Iveskite studento namu darbu rezultatus. Jei visus rezultatus suvedete iveskite '-1'" << endl;
+                while (1)
+                {
+                    cin >> skc;
+                    if (skc > 10 || skc < -1)
+                    {
+                        cout << "Tokio pazymio nera, rasykite toliau " << endl;
+                    }
+                    if (a == 0 && skc == 0)
+                    {
+                        cin.clear();
+                        cin.ignore(500, '\n');
+                        cout << "KLAIDA. Iveskite tinkama skaiciu " << endl;
+                    }
+                    else
+                    {
+                        if (!cin.fail() && skc <= 10 && skc >= 1 && skc == (int)skc)
+                        {
+                            M[i].SetPazymys(skc);
+                            a++;
+                            kiekis += skc;
+                        }
+                        else if (skc != (int)skc)cout << "KLAIDA. Iveskite naturalujy skaiciu" << endl;
+                    }
+                    if (cin.fail() || skc == 0)
+                    {
+                        cin.clear();
+                        cin.ignore(500, '\n');
+                        cout << "KLAIDA. Iveskite tinkama skaiciu " << endl;
+                    }
+                    if (a != 0 && skc == -1)
+                        break;
+                }
+            }
+            else {
+                cout << "Kiek norite kad studentas turetu pazymiu? " << endl;
+                double n;
+                while (1)
+                {
+                    cin >> n;
+                    if (!cin.fail() && n > 0 && n == (int)n)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        cin.clear();
+                        cin.ignore(500, '\n');
+                        cout << "KLAIDA. skaicius negalimas ";
+                    }
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    a++;
+                    M[i].SetPazymys(rand() % 10 + 1);
+                    cout << M[i].GetPazymys(j) << endl;
+                    kiekis += M[i].GetPazymys(j);
+                }
+            }
+            cout << "Ar norite sugeneruoti egzamino rezultata?" << endl;
+            string klausimas2;
+            while (1)
+            {
+                cin >> klausimas2;
+                if (klausimas2 == "taip" || klausimas2 == "ne")
+                {
+                    break;
+                }
+                else {
+                    cin.clear();
+                    cin.ignore(500, '\n');
+                    cout << "KLAIDA. Irasykite 'taip' arba 'ne' " << endl;
+                }
+            }
+            double egzaminas;
+            if (klausimas2 == "ne")
+            {
+                cout << "Iveskite studento egzamino rezultata" << endl;
+                while (1)
+                {
+                    cin >> egzaminas;
+                    if (!cin.fail() && egzaminas <= 10 && egzaminas == (int)egzaminas && egzaminas > 0 && egzaminas == (int)egzaminas)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        cin.clear();
+                        cin.ignore(500, '\n');
+                        cout << "KLAIDA. Iveskite tinkama egzamino rezultata desimtbaleje sistemoje " << endl;
+                    }
+                }
+
+            }
+            else if (klausimas2 == "taip")
+            {
+                egzaminas = rand() % 10 + 1;
+                cout << egzaminas << endl;
+            }
+            if (MV == 'V')
+                M[i].SetGalutinis(0.4 * (kiekis / a) + (0.6 * egzaminas));
+            else if (MV == 'M')
+            {
+                M[i].Sort();
+                // sort(M[i].pazymys.begin(), M[i].pazymys.end());
+                if (a % 2 != 0)
+                {
+                    int k = 0;
+                    k = a / 2;
+                    a -= k;
+                    mediana = M[i].GetPazymys(a - 1);
+                }
+                else
+                {
+                    int k = 0;
+                    k = a / 2;
+                    mediana = (M[i].GetPazymys(k - 1) + M[i].GetPazymys(k)) / 2;
+                }
+                M[i].SetGalutinis((0.4 * mediana) + (0.6 * egzaminas));
+            }
+            cout << "Ar yra daugiau studentu? Jei taip rasykite 'taip' " << endl;
+            string klausimas;
+            cin >> klausimas;
+            if (klausimas == "taip")
+            {
+                x++;
+                a = 0;
+                kiekis = 0;
+            }
+        }
+    }
+    string rikiavimas;
+    if (nuskaitymas5 == false)
+    {
+        cout << "Kaip norite surikiuoti sarasa pagal vardus ar pavardes?" << endl;
+        while (1)
+        {
+            cin >> rikiavimas;
+            if (rikiavimas == "vardus" || rikiavimas == "pavardes")
+            {
+                break;
+            }
+            else {
+                cin.clear();
+                cin.ignore(500, '\n');
+                cout << "KLAIDA. Pasirinkite ar rikiuoti pagal vardus ar pavardes" << endl;
+            }
+
+        }
+        if (rikiavimas == "vardus")
+            Rikiuoti(M, x);
+        else Rikiuoti1(M, x);
+    }
+    if (nuskaitymas5 == false)
+    {
+        fr << "Pavarde";
+        fr << setw(14);
+        fr << "Vardas";
+        fr << setw(24);
+        if (MV == 'M')
+        {
+            fr << "Galutinis (Med.)" << endl;
+            for (int i = 0; i < x; i++)
+            {
+                fr << M[i].GetPavarde();
+                fr << setw(14);
+                fr << M[i].GetVardas();
+                fr << setw(20);
+                fr << fixed << setprecision(2) << M[i].GetGalutinis() << endl;
+            }
+        }
+        else if (MV == 'V')
+        {
+            fr << "Galutinis(Vid.)" << endl;
+            for (int i = 0; i < x; i++)
+            {
+                fr << M[i].GetPavarde();
+                fr << setw(14);
+                fr << M[i].GetVardas();
+                fr << setw(20);
+                fr << fixed << setprecision(2) << M[i].GetGalutinis() << endl;
+            }
+        }
+    }
+}
---
+++ Files/Deque/v03.cpp
@@ -0,0 +1,252 @@
+#include "v03.h"
+void Gen(int& rekursija, int k, string failas0, int namudarbuskc, std::mt19937& mt)
+{
+    rekursija++;
+    ofstream fr(failas0);
+    std::uniform_int_distribution<int> dist(1, 10);
+    fr << left << setw(21) << "Vardas" << setw(22) << "Pavarde";
+    for (int i = 0; i < namudarbuskc; i++)
+    {
+        fr << "ND" << left << setw(18) << i + 1;
+    }
+    fr << "Egz." << endl;
+    for (int i = 0; i < k; i++)
+    {
+        fr << "Vardas" << left << setw(15) << i + 1;
+        fr << "Pavarde" << left << setw(15) << i + 1;
+        fr << setw(20);
+        for (int i = 0; i < namudarbuskc; i++) {
+            fr << dist(mt) << setw(20);
+        }
+        fr << dist(mt) << endl;
+    }
+    if (rekursija == 2)
+    {
+        failas0 = "Antras.txt";
+        Gen(rekursija, k * 10, failas0, namudarbuskc, mt);
+    }
+    else if (rekursija == 3)
+    {
+        failas0 = "Trecias.txt";
+        Gen(rekursija, k * 10, failas0, namudarbuskc, mt);
+    }
+    else if (rekursija == 4)
+    {
+        failas0 = "Ketvirtas.txt";
+        Gen(rekursija, k * 10, failas0, namudarbuskc, mt);
+    }
+    else if (rekursija == 5)
+    {
+        failas0 = "Penktas.txt";
+        Gen(rekursija, k * 10, failas0, namudarbuskc, mt);
+    }
+}
+int countWords(string str)
+{
+    // breaking input into word using string stream
+    stringstream s(str); // Used for breaking words
+    string word; // to store individual words
+
+    int count = 0;
+    while (s >> word)
+        count++;
+    return count;
+}
+bool Rik(pazymiai& a, pazymiai& b)
+{
+    return a.GetGalutinis() < b.GetGalutinis();
+}
+bool Testas(pazymiai& a, pazymiai& b)
+{
+    return a.GetGalutinis() > b.GetGalutinis();
+}
+void Skirstymas(int x, deque <pazymiai>& M, deque <pazymiai>& N, int& N1, int& B1)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    sort(M.begin(), M.end(), Testas);
+    for (int i = 0; i < M.size(); i++)
+    {
+
+        if (M[i].GetGalutinis() < 5)
+        {
+
+            N.push_back(M[i]);
+            N1++;
+            // M.erase(M.begin() + i);
+             //i--;
+        }
+    }
+    M.resize(M.size() - N.size());
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    std::cout << x << " Rusiavimas i 2 grupes: " << diff.count() << endl;
+}
+void Spausdintuvas(string ats, string ats1, int N1, char MV, int x, deque <pazymiai>& N, int B1, deque <pazymiai>& M)
+{
+    ofstream Ats(ats);
+    ofstream Ats1(ats1);
+    sort(N.begin(), N.end(), Rik);
+    Ats << "Pavarde";
+    Ats << setw(14);
+    Ats << "Vardas";
+    Ats << setw(24);
+
+    if (MV == 'M')
+    {
+        Ats << "Galutinis (Med.)" << endl;
+        for (int i = 0; i < N1; i++)
+        {
+            Ats << N[i].GetPavarde();
+            Ats << setw(14);
+            Ats << N[i].GetVardas();
+            Ats << setw(20);
+            Ats << fixed << setprecision(2) << N[i].GetGalutinis() << endl;
+        }
+    }
+    else if (MV == 'V')
+    {
+        Ats << "Galutinis(Vid.)" << endl;
+        for (int i = 0; i < N1; i++)
+        {
+            Ats << N[i].GetPavarde();
+            Ats << setw(14);
+            Ats << N[i].GetVardas();
+            Ats << setw(20);
+            Ats << fixed << setprecision(2) << N[i].GetGalutinis() << endl;
+        }
+    }
+    sort(M.begin(), M.end(), Rik);
+    Ats1 << "Pavarde";
+    Ats1 << setw(14);
+    Ats1 << "Vardas";
+    Ats1 << setw(24);
+
+    if (MV == 'M')
+    {
+        Ats1 << "Galutinis (Med.)" << endl;
+        for (int i = 0; i < x - N1; i++)
+        {
+            Ats1 << M[i].GetPavarde();
+            Ats1 << setw(14);
+            Ats1 << M[i].GetVardas();
+            Ats1 << setw(20);
+            Ats1 << fixed << setprecision(2) << M[i].GetGalutinis() << endl;
+        }
+    }
+    else if (MV == 'V')
+    {
+        Ats1 << "Galutinis(Vid.)" << endl;
+        for (int i = 0; i < x - N1; i++)
+        {
+            Ats1 << M[i].GetPavarde();
+            Ats1 << setw(14);
+            Ats1 << M[i].GetVardas();
+            Ats1 << setw(20);
+            Ats1 << fixed << setprecision(2) << M[i].GetGalutinis() << endl;
+        }
+    }
+
+}
+void Nuskaitymas(string Gavimas, string dummyline, string line, deque <pazymiai>& M, int& a, char MV, int& x, deque <pazymiai>& N, int& N1, int& B1, string ats, string ats1, int& rekursija2)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    ifstream Imimas(Gavimas);
+    getline(Imimas, dummyline);
+    string Kint;
+    while (getline(Imimas, line))
+    {
+        stringstream ss(line);
+        int numOfWords = countWords(line) - 3;
+        int exam, suma = 0;
+        double mediana = 0;
+        M.push_back(pazymiai());
+        ss >> Kint;
+        M[a].SetPavarde(Kint);
+        ss >> Kint;
+        M[a].SetVardas(Kint);
+        int temp;
+        for (int i = 0; i < numOfWords; i++)
+        {
+            ss >> temp;
+            suma += temp;
+            M[a].SetPazymys(temp);
+        }
+        ss >> exam;
+        if (MV == 'V')
+            M[a].SetGalutinis(0.4 * (suma / numOfWords) + (0.6 * exam));
+        else if (MV == 'M')
+        {
+            M[a].Sort();
+            if (numOfWords % 2 != 0)
+            {
+                int k = 0, kint;
+                k = numOfWords / 2;
+                kint = numOfWords;
+                kint -= k;
+                mediana = M[a].GetPazymys(kint - 1);
+            }
+            else
+            {
+                int k = 0;
+                k = numOfWords / 2;
+                mediana = (M[a].GetPazymys(k - 1) + M[a].GetPazymys(k)) / 2;
+            }
+            M[a].SetGalutinis((0.4 * mediana) + (0.6 * exam));
+        }
+        a++;
+        x = a;
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    std::cout << a << " Duomenu nuskaitymas: " << diff.count() << endl;
+    Skirstymas(x, M, N, N1, B1);
+    Spausdintuvas(ats, ats1, N1, MV, x, N, B1, M);
+    N.clear();
+    M.clear();
+    a = 0;
+    N1 = 0; B1 = 0;
+    rekursija2++;
+    if (rekursija2 == 2)
+    {
+        ats = "Atsakymai2.1.txt";
+        ats1 = "Atsakymai2.2.txt";
+        Gavimas = "Antras.txt";
+        Nuskaitymas(Gavimas, dummyline, line, M, a, MV, x, N, N1, B1, ats, ats1, rekursija2);
+    }
+    if (rekursija2 == 3)
+    {
+        ats = "Atsakymai3.1.txt";
+        ats1 = "Atsakymai3.2.txt";
+        Gavimas = "Trecias.txt";
+        Nuskaitymas(Gavimas, dummyline, line, M, a, MV, x, N, N1, B1, ats, ats1, rekursija2);
+    }
+    if (rekursija2 == 4)
+    {
+        ats = "Atsakymai4.1.txt";
+        ats1 = "Atsakymai4.2.txt";
+        Gavimas = "Ketvirtas.txt";
+        Nuskaitymas(Gavimas, dummyline, line, M, a, MV, x, N, N1, B1, ats, ats1, rekursija2);
+    }
+    if (rekursija2 == 5)
+    {
+        ats = "Atsakymai5.1.txt";
+        ats1 = "Atsakymai5.2.txt";
+        Gavimas = "Penktas.txt";
+        Nuskaitymas(Gavimas, dummyline, line, M, a, MV, x, N, N1, B1, ats, ats1, rekursija2);
+    }
+
+}
+void Rikiuoti(deque <pazymiai> M, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (M[i].GetVardas() > M[j].GetVardas())
+                swap(M[i], M[j]);
+}
+void Rikiuoti1(deque <pazymiai> M, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (M[i].GetPavarde() > M[j].GetPavarde())
+                swap(M[i], M[j]);
+}
