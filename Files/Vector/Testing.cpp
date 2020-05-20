99253d986a4193257070f9e8d86f4ea1e2c3290d
Kristukas <60770967+Kristukas@users.noreply.github.com>
2020-05-20 22:06:53 +0300

    Update Testing.cpp

--- Files/Vector/Testing.cpp
+++ Files/Vector/Testing.cpp
@@ -2,8 +2,8 @@
 #include "v03.h"
 void Testukas(void)
 {
-	TEST_CHECK_(countWords("testavimas pirmas") == 17, "countWords('testavimas pirmas')==17");
-	TEST_CHECK_(countWords("banditas") ==8 , "countWords('banditas')==8");
-	TEST_CHECK_(countWords("nusikaltelis") == 12, "countWords('nusikaltelis')==12");
+	TEST_CHECK_(countWords("testavimas pirmas") == 2, "countWords('testavimas pirmas')==2");
+	TEST_CHECK_(countWords("banditas") ==1 , "countWords('banditas')==1");
+	TEST_CHECK_(countWords("") == 0, "countWords('')==0");
 }
 TEST_LIST = { {"int countWords(string );", Testukas },{0} };
